a321e7d1c0d6dbd9b16a79f1a2b8c978d90d7291
Ajinkya Shinde <sajinkya359@gmail.com>
2021-11-03 11:01:54 +0530

    created theatre database

--- src/Server/database/scheme.sql
+++ src/Server/database/scheme.sql
@@ -1,9 +1,17 @@
 use database bookMyTicket;
 CREATE TABLE users (
     ID int NOT NULL auto_increment,
-    username varchar(20) NOT NULL,
-    email varchar(20) NOT NULL,
-    password varchar(20) NOT NULL,
-    mobile_no varchar(20) ,
+    username varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    mobile_no varchar(255) ,
     PRIMARY KEY (ID)
 ); 
+CREATE TABLE theatres(
+    ID int NOT NULL auto_increment,
+    theatre_name varchar(255) NOT NULL;
+    email varchar(255) NOT NULL,
+    mobile_no varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY(ID)
+)

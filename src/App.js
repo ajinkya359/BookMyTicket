87bd891bead88cff9117a3dd1d2a7cdecd8ef915
pradumn <pradumnprasad883@gmail.com>
2021-11-06 22:21:54 +0530

    Added about us page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AboutUs from "./Components/About-us/about-us.js";
 // import HeadBar from "./Components/HeadBar";
 // import Body from "./Components/Body";
 // import MovieCard from "./Components/MovieCard";
 // import HomePage from "./Components/HomePage/HomePage";
-// import TheatreListOfChosenMovie from "./Components/TheatreShowOnMovieSelect/TheatreListOfChosenMovie";
+// // import TheatreListOfChosenMovie from "./Components/TheatreShowOnMovieSelect/TheatreListOfChosenMovie";
 // import DateAndTime from "./Components/TheatreShowOnMovieSelect/DateAndTime";
-// import TheatreDetailsAndMovieTime from "./Components/TheatreShowOnMovieSelect/TheatreDetailsAndMovieTime";
-import TheatreList from "./Components/TheatreShowOnMovieSelect/TheatreList";
+// // import TheatreDetailsAndMovieTime from "./Components/TheatreShowOnMovieSelect/TheatreDetailsAndMovieTime";
+// import TheatreList from "./Components/TheatreShowOnMovieSelect/TheatreList";
 
 function App() {
   // const [signInVisible, changeSignInVisible] = useState(true);
@@ -24,19 +25,21 @@ function App() {
  
   return (
     <div>
-      {/* <HomePage /> */}
-      {/* <HeadBar showSignIn={showSignIn} showRegister={showRegister} />
-      <Body signIn={signInVisible} register={registerVisible} /> */}
-      {/* <TheatreListOfChosenMovie movie_id={1}/> */}
-      {/* <DateAndTime/> */}
-      {/* <TheatreDetailsAndMovieTime />
+{/*         
+         <HomePage /> 
+     <HeadBar showSignIn={showSignIn} showRegister={showRegister} />
+      <Body signIn={signInVisible} register={registerVisible} /> 
+       <TheatreListOfChosenMovie movie_id={1}/> 
+       <DateAndTime/> 
+       <TheatreDetailsAndMovieTime />
       <TheatreDetailsAndMovieTime />
       <TheatreDetailsAndMovieTime />
       <TheatreDetailsAndMovieTime />
-      <TheatreDetailsAndMovieTime /> */}
-      <TheatreList movie_id={4}/>
-    </div>
-  );
+      <TheatreDetailsAndMovieTime /> 
+      <TheatreList movie_id={4}/>   */}
+      <AboutUs/>
+      </div>
+      );
 }
 
 export default App;

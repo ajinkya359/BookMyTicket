3e4d20a5cfaff4f8b12332b23535ab6ba8bb87ac
pradumn <pradumnprasad883@gmail.com>
2021-11-20 22:04:47 +0530

    Added Seat Selection Page

---
+++ src/Components/SelectSeatPage/SeatStatus.js
@@ -0,0 +1,235 @@
+import React from "react";
+import "./SeatStatus.css"
+class Seats extends React.Component
+{
+    render()
+    {
+        return (
+          
+          <div class="wrapper">
+	<div class="container">
+       <div class="screen">All eyes this way please.</div>
+       <div class="seats-status">
+           <table>
+               <tr>
+                   <td class="row">A</td>
+                   <td class="real-seat">
+                       <div class="seate"></div>
+                       <div class="seate"></div>
+                       <div class="seat">1</div>
+                       <div class="seat">2</div>
+                       <div class="seat">3</div>
+                       <div class="seat">4</div>
+                       <div class="seat">5</div>
+                       <div class="seat">6</div>
+                       <div class="seate"></div>
+                       <div class="seate"></div>
+                       <div class="seate"></div>
+                       <div class="seate"></div>
+                       <div class="seat">7</div>
+                       <div class="seat">8</div>
+                       <div class="seat">9</div>
+                       <div class="seat">10</div>
+                       <div class="seat">11</div>
+                       <div class="seat">12</div>
+                       <div class="seate"></div>
+                       <div class="seate"></div>
+                    </td>
+               </tr>
+               <tr>
+                <td class="row">B</td>
+                <td class="real-seat">
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">1</div>
+                    <div class="seat">2</div>
+                    <div class="seat">3</div>
+                    <div class="seat">4</div>
+                    <div class="seat">5</div>
+                    <div class="seat">6</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">7</div>
+                    <div class="seat">8</div>
+                    <div class="seat">9</div>
+                    <div class="seat">10</div>
+                    <div class="seat">11</div>
+                    <div class="seat">12</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                 </td>
+               </tr>
+               <tr>
+                <td class="row">C</td>
+                <td class="real-seat">
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">1</div>
+                    <div class="seat">2</div>
+                    <div class="seat">3</div>
+                    <div class="seat">4</div>
+                    <div class="seat">5</div>
+                    <div class="seat">6</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">7</div>
+                    <div class="seat">8</div>
+                    <div class="seat">9</div>
+                    <div class="seat">10</div>
+                    <div class="seat">11</div>
+                    <div class="seat">12</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                 </td>
+               </tr>
+               <tr>
+                <td class="row">D</td>
+                <td class="real-seat">
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">1</div>
+                    <div class="seat">2</div>
+                    <div class="seat">3</div>
+                    <div class="seat">4</div>
+                    <div class="seat">5</div>
+                    <div class="seat">6</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">7</div>
+                    <div class="seat">8</div>
+                    <div class="seat">9</div>
+                    <div class="seat">10</div>
+                    <div class="seat">11</div>
+                    <div class="seat">12</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                 </td>
+               </tr>
+             
+            <tr>
+             <td></td>
+              <td> <div class="lable">EXECUTIVE-Rs. 130.00</div></td>
+          
+            </tr>
+               <tr>
+                <td class="row">E</td>
+                <td class="real-seat">
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">1</div>
+                    <div class="seat">2</div>
+                    <div class="seat">3</div>
+                    <div class="seat">4</div>
+                    <div class="seat">5</div>
+                    <div class="seat">6</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">7</div>
+                    <div class="seat">8</div>
+                    <div class="seat">9</div>
+                    <div class="seat">10</div>
+                    <div class="seat">11</div>
+                    <div class="seat">12</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                 </td>
+               </tr>
+               <tr>
+                <td class="row">F</td>
+                <td class="real-seat">
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">1</div>
+                    <div class="seat">2</div>
+                    <div class="seat">3</div>
+                    <div class="seat">4</div>
+                    <div class="seat">5</div>
+                    <div class="seat">6</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">7</div>
+                    <div class="seat">8</div>
+                    <div class="seat">9</div>
+                    <div class="seat">10</div>
+                    <div class="seat">11</div>
+                    <div class="seat">12</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                 </td>
+               </tr>
+               <tr>
+                <td class="row">G</td>
+                <td class="real-seat">
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">1</div>
+                    <div class="seat">2</div>
+                    <div class="seat">3</div>
+                    <div class="seat">4</div>
+                    <div class="seat">5</div>
+                    <div class="seat">6</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">7</div>
+                    <div class="seat">8</div>
+                    <div class="seat">9</div>
+                    <div class="seat">10</div>
+                    <div class="seat">11</div>
+                    <div class="seat">12</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                 </td>
+               </tr>
+               <tr>
+                <td class="row">H</td>
+                <td class="real-seat">
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">1</div>
+                    <div class="seat">2</div>
+                    <div class="seat">3</div>
+                    <div class="seat">4</div>
+                    <div class="seat">5</div>
+                    <div class="seat">6</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                    <div class="seat">7</div>
+                    <div class="seat">8</div>
+                    <div class="seat">9</div>
+                    <div class="seat">10</div>
+                    <div class="seat">11</div>
+                    <div class="seat">12</div>
+                    <div class="seate"></div>
+                    <div class="seate"></div>
+                 </td>
+               </tr>
+               <tr>
+               <td></td>
+               <td><div class="lable">PREMIUM-Rs. 300.00</div></td>
+                
+           
+             </tr>
+           </table>
+        
+       </div> 
+    </div>
+</div>
+ );
+    }
+}
+
+export default Seats;
